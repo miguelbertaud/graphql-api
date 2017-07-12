fe790a9b8fba09137031ecf4f69dc9b76ca9f634
Miguel Bertaud <miguel.bertaud@gmail.com>
2017-07-12 12:45:05 -0500

    Add root property for graphql

---
+++ app/graphql/graphql_tutorial_schema.rb
@@ -0,0 +1,3 @@
+GraphqlTutorialSchema = GraphQL::Schema.define do
+  query(Types::QueryType)
+end
--- config/application.rb
+++ config/application.rb
@@ -15,10 +15,10 @@ require "rails/test_unit/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
 module GraphqlTutorial
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.autoload_paths << Rails.root.join("app", "types")
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
